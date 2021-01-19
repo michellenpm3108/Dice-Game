babc92fb48f77048dbb8d987e950d9e63b70321e
Michelle <michelle.npm3108@gmail.com>
2021-01-20 05:22:48 +0800

    Update index.js
    make the game fair to player 2

--- index.js
+++ index.js
@@ -12,56 +12,56 @@ const message = document.getElementById('message')
 let player1Score = 0
 let player2Score = 0
 let player1Turn = true
+rollBtn.addEventListener('click', function(){
+    randomScore = Math.floor(Math.random()*6) + 1;
+    if (player1Turn) {
+        player1Dice.innerHTML = randomScore;
+        player1Score += randomScore;
+        player1Scoreboard.innerHTML = player1Score;
+        player1Turn = false;
+        message.innerHTML = "Next: Player 2 Turn";
+        player2Dice.classList.remove('active');
+        player1Dice.classList.add('active');
+    } else {
+        player2Dice.innerHTML = randomScore;
+        player2Score += randomScore;
+        player2Scoreboard.innerHTML = player2Score;
+        player1Turn = true;
+        message.innerHTML = "Next: Player 1 Turn";
+        player1Dice.classList.remove('active');
+        player2Dice.classList.add('active');
+    }
 
-function displayBtn() {
-    rollBtn.style.display = 'none';
-    resetBtn.style.display = 'block';
-} 
-
-
-rollBtn.addEventListener('click', function() {
- let numberRandom = Math.floor(Math.random()*6) +1
- if (player1Turn) {
-     player1Dice.innerHTML = numberRandom;
-     player1Score += numberRandom;
-     player1Scoreboard.innerHTML = player1Score;
-     player2Dice.classList.remove('active');
-     player1Dice.classList.add('active');
-     message.innerHTML = 'NEXT: Player 2 turn'
- } else {
-    player2Dice.innerHTML = numberRandom;
-    player2Score += numberRandom;
-    player2Scoreboard.innerHTML = player2Score;
-    player1Dice.classList.remove('active');
-    player2Dice.classList.add('active');
-    message.innerHTML = 'NEXT: Player 1 turn'
- }
-
-player1Turn = !player1Turn;
-
-if (player1Score >= 20) {
-    message.innerHTML = 'Player 1 has won!!!'
-    displayBtn() 
-}
-if (player2Score >= 20) {
-    message.innerHTML = 'Player 2 has won!!!'
-    displayBtn() 
-} 
+    if (player1Score >=20) {
+        message.innerHTML = "Player 1 has won!!";
+        rollBtn.style.display="none";
+        resetBtn.style.display="block";
+        player1Turn = true;
+    }
 
 
+    if (player2Score >=20) {
+        message.innerHTML = "Player 2 has won!!";
+        rollBtn.style.display="none";
+        resetBtn.style.display="block";
+        player1Turn = false;
+    }
 })
 
-resetBtn.addEventListener('click', function reset(){
-    resetBtn.style.display = "none";
-    rollBtn.style.display = "block";
-    player1Scoreboard.innerHTML='0';
-    player2Scoreboard.innerHTML='0';
-    player1Dice.innerHTML='-';
-    player2Dice.innerHTML='-';
+resetBtn.addEventListener('click', function(){
+    resetBtn.style.display="none";
+    rollBtn.style.display="block";
+    player1Dice.innerHTML="-";
     player1Score = 0;
+    player1Scoreboard.innerHTML ="0";
+    player2Dice.innerHTML="-";
     player2Score = 0;
-    message.innerHTML = "Player 1 turn";     player2Dice.classList.remove('active');
-    player2Dice.classList.remove('active');
-    player1Dice.classList.remove('active');
+    player2Scoreboard.innerHTML ="0";
 
+    if (player1Turn) {
+        message.innerHTML = "Player 1 Turn First"
+    } else {
+        message.innerHTML = "Player 2 Turn First"
+    }
 })
+
